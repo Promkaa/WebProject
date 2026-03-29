5c63d161e54b22912c0cb6a2fde61c2a2d37e128
Promkaa <goshikonelegal@gmail.com>
2026-03-29 16:38:46 +0700

    modified:   frontend/src/App.js 	modified:   frontend/src/css/main.css 	modified:   frontend/src/pages/login.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
-// import Login from './pages/login'
-import './index'
-import './css/main.css'
+import Login from './pages/login';
 import MainStr from './pages/main';
-//  <-- вставлять нужное ниже в <div>
-class App extends React.Component{
-  render(){return(<div>
-    <MainStr />
-  
-  </div>
-  )
-}
+import './index';
+import './css/main.css';
+
+class App extends React.Component {
+  state = {
+    isLoggedIn: false  // Состояние авторизации
+  };
+
+  // Функция для входа
+  handleLogin = () => {
+    // Здесь можно добавить проверку логина/пароля
+    this.setState({ isLoggedIn: true });
+  };
+
+  // Функция для выхода
+  handleLogout = () => {
+    this.setState({ isLoggedIn: false });
+  };
+
+  render() {
+    const { isLoggedIn } = this.state;
+
+    return (
+      <div>
+        {isLoggedIn ? (
+          <MainStr onLogout={this.handleLogout} />  // Передаем функцию выхода
+        ) : (
+          <Login onLogin={this.handleLogin} />       // Передаем функцию входа
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,72 +1,81 @@
 import React from "react";
+import '../css/main.css'
 
-class Login extends React.Component{
-    state = {
-        login: "",
-        password: "",
-        remember: false
-    };
+class Login extends React.Component {
+  state = {
+    login: "",
+    password: "",
+    remember: false
+  };
 
-    handleInputChange = (e) => {
+  handleInputChange = (e) => {
     const { name, value, type, checked } = e.target;
     this.setState({
       [name]: type === "checkbox" ? checked : value
     });
   };
 
-    handleSubmit = (e) => {
-        e.preventDefault();
-        console.log("Form submitted:", this.state);
-    };
-
-    render(){
-        return(
-            <div className="login">
-                <center><header>Привет</header></center>
-                <form onSubmit={this.handleSubmit}>
-                    <h1>Авторизация</h1>
-                    <div className="input-box">
-                    <input 
-                    type="text" 
-                    name="login"
-                    placeholder="Введите логин" 
-                    value={this.state.login}
-                    onChange={this.handleInputChange}
-                    required />
-                    </div>
-
-                    <div className="input-box">
-                    <input 
-                    type="password"
-                    name="password" 
-                    placeholder="Введите пароль" 
-                    value={this.state.password}
-                    onChange={this.handleInputChange}
-                    required />
-                    </div>
-
-                    <div className="remember">
-                    <input 
-                    type="checkbox" 
-                    name="remember"
-                    checked={this.state.remember}
-                    onChange={this.handleInputChange}
-                    />Запомнить меня (не запомним)
-                    </div>
-
-                    
-
-                    <button type="submit">Войти</button>
-
-                    <div className="register-link">
-                        <center><p>Нет аккаунта?</p></center>
-                    </div>
-
-                </form>
-            </div>
-        )
+  handleSubmit = (e) => {
+    e.preventDefault();
+    
+    if (this.state.login && this.state.password) {
+      console.log("Form submitted:", this.state);
+      
+      if (this.props.onLogin) {
+        this.props.onLogin();
+      }
+    } else {
+      alert("Пожалуйста, введите логин и пароль");
     }
+  };
+
+  render() {
+    return (
+      <div className="login">
+        
+        <form onSubmit={this.handleSubmit}>
+            <center><header>Привет</header></center>
+          <h1>Авторизация</h1>
+          <div className="input-box">
+            <input 
+              type="text" 
+              name="login"
+              placeholder="Введите логин" 
+              value={this.state.login}
+              onChange={this.handleInputChange}
+              required 
+            />
+          </div>
+
+          <div className="input-box">
+            <input 
+              type="password"
+              name="password" 
+              placeholder="Введите пароль" 
+              value={this.state.password}
+              onChange={this.handleInputChange}
+              required 
+            />
+          </div>
+
+          <div className="remember">
+            <input 
+              type="checkbox" 
+              name="remember"
+              checked={this.state.remember}
+              onChange={this.handleInputChange}
+            />Запомнить меня (не запомним)
+          </div>
+
+          <button type="submit">Войти</button>
+
+          <div className="register-link">
+            <center><p>Нет аккаунта?</p></center>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Login
-
+export default Login;
